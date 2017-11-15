1daf1d49dfcc9f8fbb2f26de50c874e990891303
Scott-Buglass-GSU <scott_m_buglass@georgiasouthern.edu>
2017-11-15 12:47:17 -0500

    Added GUI for Adding New Trackers
    Pops up a page in the center of the screen if the button tag is "empty"
    added a switch case so that we can later have it open different pages
    depending on the tag and type of button

--- HCI_Project/MainWindow.xaml.cs
+++ HCI_Project/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace HCI_Project
 {
@@ -20,9 +21,20 @@ namespace HCI_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button[] TrackerButtons;
+
         public MainWindow()
         {
             InitializeComponent();
+            TrackerButtons = new Button[8];
+            TrackerButtons[0] = btnTracker0;
+            TrackerButtons[1] = btnTracker1;
+            TrackerButtons[2] = btnTracker2;
+            TrackerButtons[3] = btnTracker3;
+            TrackerButtons[4] = btnTracker4;
+            TrackerButtons[5] = btnTracker5;
+            TrackerButtons[6] = btnTracker6;
+            TrackerButtons[7] = btnTracker7;
         }
 
         private void btnPreferences_Click(object sender, RoutedEventArgs e)
@@ -34,6 +46,26 @@ namespace HCI_Project
         private void mainFrame_ContentRendered(object sender, EventArgs e)
         {
             mainFrame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+        }
+
+        private void btnTracker_Click(object sender, RoutedEventArgs e)
+        {
+            //pull button and grid from form
+            Button button = sender as Button;
+            Frame frm = popTrackerPop.Child as Frame;
+
+            switch (button.Tag)
+            {
+                case "empty":
+                    frm.Navigate(new AddTrackerPage(popTrackerPop));
+                    break;
+                default:
+                    break;
+
+            }
+
+            popTrackerPop.IsOpen = true;
+
         }
     }
 }
